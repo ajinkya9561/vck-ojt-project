15eca3ed6cf5ea6dd1edcf78a0cde8c47b5c727f
Ajinkya Undale <ajinkyaundale20@gmail.com>
2025-08-16 13:49:33 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 
 import { Link } from "react-router-dom";
-import Header from "../components/HEADER/Header";
+import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
 const HomePage = () => {
