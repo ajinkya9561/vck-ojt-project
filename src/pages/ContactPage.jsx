144b11815d48b7edafae815df673a02b5a77b092
Ajinkya Undale <ajinkyaundale20@gmail.com>
2025-08-16 13:50:27 +0530

    Update ContactPage.jsx

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,5 @@
 import Footer from "../components/Footer/Footer";
-import Header from "../components/HEADER/Header";
+import Header from "../components/Header/Header";
 
 
 const ContactPage = () => {
